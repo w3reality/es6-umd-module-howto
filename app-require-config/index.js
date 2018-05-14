96168d89510d0d9b567653c8158d102eb141b947
j <j@example.com>
2018-05-14 11:45:21 +0700

    examples with requirejs

---
+++ app-require-config/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var MyModule = require('my-module');
+// console.log(MyModule); // {Foo: ƒ, Bar: ƒ}
+var foo = new MyModule.Foo(true);
+var bar = new MyModule.Bar(true);
+foo.hello();
+bar.hello();
